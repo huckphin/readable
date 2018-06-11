1cdf6a830a08b1e92946372391a9b21c94cfb257
Chad Rhyner <chadrhyner@gmail.com>
2018-06-10 20:06:11 -0400

    remove an unused variable: compose

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore, compose } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducers';
 
 const store = createStore(
